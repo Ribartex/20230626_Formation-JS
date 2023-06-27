2b7629d20922c91608ee1c33b39484b11f332a81
Ribartex <Ribartex@gmail.com>
2023-06-27 10:55:28 +0200

    theme dark/light

--- JS/index.js
+++ JS/index.js
@@ -15,7 +15,22 @@ function init(){
     footer.style.textDecoration='underline grey';
     footer.style.textUnderlineOffset='3pt';
 }
-
+/**
+ * changement d'etat du theme
+ * @param {boolean} isDark etat du choix de theme dark/clear
+ */
+function changeTheme(isDark) {
+    var nav=document.getElementsByTagName('nav')[0]
+    if (isDark) {
+        document.body.className = "dark";
+        nav.classList.replace('navbar-light','navbar-dark');
+        nav.classList.replace('bg-light','bg-dark');
+    } else {
+        document.body.className = "";
+        nav.classList.replace('navbar-dark','navbar-light');
+        nav.classList.replace('bg-dark','bg-light');
+    }
+}
 document.addEventListener('DOMContentLoaded',function(evt){
     console.log(evt);
     init();
