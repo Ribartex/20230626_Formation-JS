b41279e9a4f3ad4d29e6b55940a94feb5b397338
Ribartex <Ribartex@gmail.com>
2023-06-27 13:34:58 +0200

    Creation des forms pour chaque entrée d'un meme

---
+++ JS/memeform.js
@@ -0,0 +1,28 @@
+var currentMeme = new Meme("");
+ //console.log(meme);
+ function initMemeEditor() {
+    var form=document.forms['meme-form'];
+    form['titre'].addEventListener('input',function(evt){
+        currentMeme.titre=evt.target.value
+    });
+    var form=document.forms['meme-form'];
+    form['imageId'].addEventListener('change',function(evt){
+        currentMeme.imageId=evt.target.value
+    })
+    var form=document.forms['meme-form'];
+    form['text'].addEventListener('input',function(evt){
+        currentMeme.text=evt.target.value
+    })
+    var form=document.forms['meme-form'];
+    form['x'].addEventListener('change',function(evt){
+        currentMeme.x=evt.target.value
+    })
+    var form=document.forms['meme-form'];
+    form['y'].addEventListener('change',function(evt){
+        currentMeme.y=evt.target.value
+    })
+    var form=document.forms['meme-form'];
+    form['color'].addEventListener('change',function(evt){
+        currentMeme.color=evt.target.value
+    })
+ }
