dec0f1cc2e57a1952f004aef8d88ec264b534d2d
Ribartex <Ribartex@gmail.com>
2023-06-27 09:47:06 +0200

    manip balise style du footer

--- JS/index.js
+++ JS/index.js
@@ -9,6 +9,11 @@ function init(){
     console.log(currentDate.toISOString());
     var footer=document.getElementsByTagName('footer')[0]
     footer.innerHTML=currentDate.toISOString()
+    footer.style.fontStyle='italic';
+    footer.style.color='white';
+    footer.style.backgroundColor='rgba(128,128,128,0.2)';
+    footer.style.textDecoration='underline grey';
+    footer.style.textUnderlineOffset='3pt';
 }
 
 document.addEventListener('DOMContentLoaded',function(evt){
