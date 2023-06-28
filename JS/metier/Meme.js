000b1fb8d3335af1e76ab50d303941533a2ea9dc
Ribartex <Ribartex@gmail.com>
2023-06-28 15:36:30 +0200

    Meme.js

---
+++ JS/metier/Meme.js
@@ -0,0 +1,12 @@
+export class Meme {
+  titre = "";
+  text = "";
+  x = 0;
+  y = 50;
+  imageId = -1;
+  fontSize = 30;
+  fontWeight = "500";
+  underline = false;
+  italic = false;
+  color = "#000000";
+}
---
+++ JS/metier/Ressources.js
@@ -0,0 +1,7 @@
+export class Ressources{
+    #images=[]
+    #memes=[]
+    loadRessources(){
+        
+    }
+}
