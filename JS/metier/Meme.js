d2b07fd88fe615ccb87d92d8a7d33be67d946414
Ribartex <Ribartex@gmail.com>
2023-06-29 10:56:32 +0200

    ajout render

--- JS/metier/Meme.js
+++ JS/metier/Meme.js
@@ -9,4 +9,23 @@ export class Meme {
   underline = false;
   italic = false;
   color = "#000000";
+  static render(meme, cssSelector, img) {
+    const svg = document.querySelector(cssSelector + " svg");
+    svg.setAttribute(
+      "viewBox",
+      `0 0 ${undefined !== img ? img.w : 500} ${
+        undefined !== img ? img.h : 500
+      }`
+    );
+    const imgElement = svg.querySelector("image");
+    const textElement = svg.querySelector("text");
+    
+    imgElement.setAttribute("xlink:href", undefined !== img ? img.url : "");
+
+    textElement.innerHTML = meme.text;
+    textElement.style.fill = meme.color;
+    textElement.style.fontSize = meme.fontSize;
+    textElement.setAttribute("x", meme.x);
+    textElement.setAttribute("y", meme.y);
+  }
 }
